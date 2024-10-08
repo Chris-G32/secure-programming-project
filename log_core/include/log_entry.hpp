ef0dc49263cd47eee5ca36e4a2e716efc01814d8
Jhoope11 <jhoope11@kent.edu>
2024-10-08 15:53:58 -0400

    Added a version for if it has a - in the args or not.

--- log_core/include/log_entry.hpp
+++ log_core/include/log_entry.hpp
@@ -26,9 +26,9 @@ public:
         return std::make_pair(true, *_roomID);
     }
     LogEntry(std::string entry){
-        std::string time = entry.substr(0, entry.find_first_of(" "));
+        std::string time = entry.substr(entry.begin(), entry.find_first_of(" "));
         entry.erase(entry.begin(), entry.find_first_of(" "));
-        std::string isEmployee = entry.substr(0, entry.find_first_of(" "));
+        std::string isEmployee = entry.substr(entry.begin(), entry.find_first_of(" "));
         entry.erase(entry.begin(), entry.find_first_of(" "));
         std::string name = entry.substr(entry.begin(), entry.find_first_of(" "))
         entry.erase(entry.begin(), entry.find_first_of(" "));
@@ -46,13 +46,21 @@ public:
         else { LogEntry::isGuest(); }
         if(isArrival == "-A") { LogEntry::isArrival(); _roomID = std::atoi("1") }
         else if (isArrival == "-R") {_roomID = std::atoi(roomID)}
-        else {
-            
-        }
+        else { LogEntry::isArrival(); _roomID = std::atoi("1") }
+        // if(isArrival == "R"){
+        //     entry.erase(entry.begin(), entry.find_first_of(" "));
+        //     std::string roomID = entry.substr(entry.begin(), entry.end()-1);
+        // }
+        // else{
+        //     //deleting since entry should be empty at this point;
+        //     delete entry;
+        // }
+        // _time = std::stoul(time); _name = name; 
         // if(isEmployee == "E") { LogEntry::isEmployee(); }
         // else { LogEntry::isGuest(); }
         // if(isArrival == "A") { LogEntry::isArrival(); _roomID = std::atoi("1") }
         // else if (isArrival == "R") {_roomID = std::atoi(roomID)}
+        // else { LogEntry::isArrival(); _roomID = std::atoi("1") }
     }
 
     ~LogEntry() { delete _roomID; }
