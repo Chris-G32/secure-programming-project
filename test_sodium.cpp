81d6fc9d5668bcd3eca2cbcce6a9c07159bb164d
cohenstevens <csteve54@kent.edu>
2024-10-10 13:48:03 -0400

    simple executable added

---
+++ test_sodium.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include <sodium.h>
+
+TEST(SodiumTest, Initialization) {
+    ASSERT_EQ(sodium_init(), 0) << "Sodium initialization failed";
+}
+
+TEST(SodiumTest, RandomNumberGeneration) {
+    uint32_t random_number1, random_number2;
+    randombytes_buf(&random_number1, sizeof(random_number1));
+    randombytes_buf(&random_number2, sizeof(random_number2));
+    
+    // This test has a very small chance of failing if the same number is generated twice
+    EXPECT_NE(random_number1, random_number2) << "Generated numbers should be different";
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
