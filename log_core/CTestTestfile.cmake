ff0425dff82cc2b463e8f452dab35cd8227fd22a
Chris-G32 <cgruska@kent.edu>
2024-10-29 14:28:01 -0400

    remove: extra cmake trash that should be ignored

--- log_core/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/ksu-projects/secure-programming-project/log_core
-# Build directory: /home/chris/ksu-projects/secure-programming-project/log_core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gallery_tests "/home/chris/ksu-projects/secure-programming-project/log_core/gallery_tests")
-set_tests_properties(gallery_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/chris/ksu-projects/secure-programming-project/log_core/CMakeLists.txt;44;add_test;/home/chris/ksu-projects/secure-programming-project/log_core/CMakeLists.txt;0;")
